76e74bf1a4325638ea60d5eff71ed00090c75e3c
Reed Weichler <rweichler@gmail.com>
2016-09-16 21:53:45 -0700

    have beagle return lua table instead of objc thing

--- src/lua/beagle/init.lua
+++ src/lua/beagle/init.lua
@@ -12,17 +12,17 @@ local beagle = {}
 
 function beagle.beagle(class)
     local arr = lib.beagle_getInstancesOfClass(objc.class(class))
-    return arr and arr:retain()
+    return arr and objc.tolua(arr)
 end
 
 function beagle.exact(class)
     local arr = lib.beagle_getInstancesOfExactClass(objc.class(class))
-    return arr and arr:retain()
+    return arr and objc.tolua(arr)
 end
 
 function beagle.first(class)
     local arr = lib.beagle_getFirstInstanceOfClass(objc.class(class))
-    return arr and arr:retain()
+    return arr and objc.tolua(arr)
 end
 
 return setmetatable({}, {__index = beagle, __call = function(self, ...) return beagle.beagle(...) end})
